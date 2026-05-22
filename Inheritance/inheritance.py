151de5b2e8254e285d371e4b3c5b737e8ad8d6ea
nandhiniannika <150242225+nandhiniannika@users.noreply.github.com>
2026-05-22 15:38:32 +0530

    Inheritance1

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -5,6 +5,8 @@
 #     def m1(self):
 #         print("B Class")
 #         super().m1()
+from typing import override
+
 
 # obj = B()
 # obj.m2()
@@ -28,7 +30,7 @@ class D(A,B,C):
         super().m1()
 obj=D()
 obj.m1()
-print(D.mro())
+# print(D.mro())
 #a1=A()
 #a1.m1()
 
@@ -43,6 +45,24 @@ class C:
         a1 = A()
         a1.m1()
         print("C class")
-c1 = C()
-c1.m1()
+# c1 = C()
+# c1.m1()
 
+
+
+# class Animal with a method sound().Create a derived class Dog that overrides the sound() method.Demonstrate method overriding.
+
+class Animal:
+    def sound(self):
+        print("Animal Makes Sound")
+class Dog(Animal):
+    def sound(self):
+        print("woffffffff")
+
+d = Dog()
+d.sound()
+
+# • Create class A with method show().Create class B(A) that overrides show() and also calls the parent method using super().
+# • Create multi-level inheritance with classes A → B → C, each having a method display() printing the class name.Create object of C and call display(), showing method resolution.
+# • Implement hierarchical inheritance using a base class Vehicle and two child classes Car and Bike, each defining a method wheels().
+# • Create class Employee with an instance method salary().Create class Manager(Employee) that overrides salary() and adds an incentive.Demonstrate both outputs.
