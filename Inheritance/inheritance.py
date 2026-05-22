a5e61d6f5a31d9da08156376e8bac7f3438a64f7
nandhiniannika <150242225+nandhiniannika@users.noreply.github.com>
2026-05-22 16:09:58 +0530

    Inheritance1

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -29,7 +29,7 @@ class D(A,B,C):
         print("D")
         super().m1()
 obj=D()
-obj.m1()
+# obj.m1()
 # print(D.mro())
 #a1=A()
 #a1.m1()
@@ -59,10 +59,60 @@ class Dog(Animal):
     def sound(self):
         print("woffffffff")
 
-d = Dog()
-d.sound()
+# d = Dog()
+# d.sound()
 
 # • Create class A with method show().Create class B(A) that overrides show() and also calls the parent method using super().
+
+# class A:
+#     def show(self):
+#         print("A class")
+# class B(A):
+#     def show(self):
+#         print("B class")
+#         super().show()
+
+# b = B()
+# b.show()
+
 # • Create multi-level inheritance with classes A → B → C, each having a method display() printing the class name.Create object of C and call display(), showing method resolution.
+class A:
+    def display(self):
+        print("A")
+class B(A):
+    def display(self):
+        print("B")
+        super().display()
+class C(B):
+    def display(self):
+        print("C")
+        super().display()
+# c = C()
+# c.display()
+
 # • Implement hierarchical inheritance using a base class Vehicle and two child classes Car and Bike, each defining a method wheels().
+class Vehicle:
+    def wheels(self):
+        print("Wheels of Vehicles")
+class Car(Vehicle):
+    def wheels(self):
+        print("4 wheels")
+class Bike(Vehicle):
+    def wheels(self):
+        print("2 wheels")
+
+# bike = Bike()
+# bike.wheels()
+# car = Car()
+# car.wheels()
+
 # • Create class Employee with an instance method salary().Create class Manager(Employee) that overrides salary() and adds an incentive.Demonstrate both outputs.
+
+class Employee:
+    def salary(self):
+        print("50000")
+class Manager(Employee):
+    def salary(self):
+        print("30000")
+
+
